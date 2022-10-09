c88021b31c7e11776be69c05fa3008ddc7eb7ef9
Jay Liu <jliu2589@gmail.com>
2022-10-08 21:11:24 -0400

    Add basic styling, basic state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState("Start");
+  const [time, setTime] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto mt-4">
+      <div>Hooks - Prop Drillings</div>
+      <div>Stopwatch: </div>
+      <button
+        className="bg-blue-400 p-2 rounded shadow"
+        onClick={() => {
+          if (timer === "Start") {
+            setTimer("Stop");
+          }
+          if (timer === "Stop") {
+            setTimer("Start");
+          }
+          console.log("Change from Start to Stop. Activate Timer/ Stop Timer");
+        }}
+      >
+        {timer}
+      </button>
+      <div>
+        <ul>
+          <li>list</li>
+        </ul>
+      </div>
     </div>
   );
 }
