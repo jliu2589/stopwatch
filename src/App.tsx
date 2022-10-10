712014ddb30c57541bec7a58f34e52b9aec01032
Jay Liu <jliu2589@gmail.com>
2022-10-10 19:09:06 -0400

    Stopwatch ms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { time } from "console";
 
 function App() {
   const [isActive, setIsActive] = useState(false);
   const [timeMs, setTimeMs] = useState(0);
   const [timeSec, setTimeSec] = useState(0);
   const [timeMin, setTimeMin] = useState(0);
+
+  useEffect(() => {
+    let interval: any = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimeMs((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive]);
 
   const handleStart = () => {
     setIsActive(!isActive);
